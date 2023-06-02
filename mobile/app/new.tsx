014918cb50c3cc753d3908f089e415706331f384
wanderley9898 <wanderleysilva989@gmail.com>
2023-06-02 13:54:20 -0300

    cadastro de novas memorias

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,5 +1,10 @@
 import Icon from '@expo/vector-icons/Feather'
+import * as ImagePicker from 'expo-image-picker'
+import { Link, useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import { useState } from 'react'
 import {
+  Image,
   ScrollView,
   Switch,
   Text,
@@ -7,20 +12,64 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native'
-
-import { Link } from 'expo-router'
-import { useState } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 export default function NewMemories() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
+  const [preview, setPreview] = useState<string | null>(null)
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+      uploadFormData.append('file', {
+        name: 'image.jpg',
+        uri: preview,
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    await api.post(
+      '/memories',
+      { content, isPublic, coverUrl },
+      { headers: { Authorization: `Bearer ${token}` } },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1 px-6"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
       <View className="flex-row items-center justify-between">
@@ -48,18 +97,28 @@ export default function NewMemories() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={openImagePicker}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou video de capa
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou video de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
@@ -67,6 +126,7 @@ export default function NewMemories() {
 
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={handleCreateMemory}
           className="items-center self-end rounded-full bg-green-500 px-5 py-2 "
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
